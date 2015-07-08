2510363bb067cd26f540046389ce91c92de0c992
Toru Hagihara <toru.hagihara.matatavi@gmail.com>
2015-07-08 15:02:35 +0900

    Fix
    add echo

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 /*
 Initialize DB table
 */
+    echo 'Initializing DB';
     $dbUrl = parse_url(getenv('DATABASE_URL'));
     $dbName = ltrim($dbUrl['path'], '/');
     $dbHost = $dbUrl['host'];
@@ -10,8 +11,11 @@ Initialize DB table
     $dbPass = $dbUrl['pass'];
 
     $conn = "host=". $dbHost. " dbname=". $dbName . " user=" .$dbUser . " password=". $dbPass;
-    pg_connect($conn);
+    echo 'Start connecting to DB';
+    $_con = pg_connect($conn);
+    echo 'Finish connecting to DB';
 
+    echo 'Start creating table';
     $sqlresult = pg_query($_con,
      "CREATE TABLE tones(
      id SERIAL,
@@ -20,4 +24,6 @@ Initialize DB table
      done boolean default false
      );"
      );
+     echo 'Finish creating table';
+     echo 'Complite Initializing DB';
 ?>
